591e81e77fcc7d62ddfda94a67ce3b69329e31ca
Piotr Sykulski <pitersk@gmail.com>
2018-10-18 13:55:06 +0200

    Finished test scripts

--- Scripts/ff_anc_test.sh
+++ Scripts/ff_anc_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -avz --cvs-exclude RpiANC --exclude "RpiANC/cmake-build-debug" --exclude "RpiANC/cmake-build-release" --exclude "RpiANC/Makefile" --exclude "RpiANC/OctaveCode" --exclude "RpiANC/.idea" RpiANC pi@192.168.0.155:./ \
+rsync -avz --cvs-exclude ../../RpiANC --exclude "RpiANC/cmake-build-debug" --exclude "RpiANC/cmake-build-release" --exclude "RpiANC/Makefile" --exclude "RpiANC/OctaveCode" --exclude "RpiANC/.idea" ../../RpiANC pi@192.168.0.155:./ \
 && ssh pi@192.168.0.155 "cd ~/RpiANC && make ffANC" 
 mpsyt 'playurl https://www.youtube.com/watch?v=D00TqNbMDNI' &
 #mpsyt 'playurl https://www.youtube.com/watch?v=vuZ4uP279oc' &
@@ -7,5 +7,7 @@ ssh pi@192.168.0.155 "cd ~/RpiANC && ./ffANC" && \
 pkill mpsyt && \
 pkill mpv && \
 rsync -avz pi@192.168.0.155:~/RpiANC/rec/ ./audio_files
+pipenv run ./plot_test_results.py &
+pipenv run ./play_raw_audio.py audio_files/err_mic.dat
 
 
---
+++ Scripts/plot_test_results.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+error_file = "audio_files/err_mic.dat"
+reference_file = "audio_files/ref_mic.dat"
+correction_file = "audio_files/corr_sig.dat"
+
+# Load all test results files
+error_signal = np.fromfile(error_file, dtype=np.int32)
+reference_signal = np.fromfile(reference_file, dtype=np.int32)
+correction_signal = np.fromfile(correction_file, dtype=np.int32)
+
+ii32 = np.iinfo(np.int32)
+
+error_signal = error_signal.astype(np.float64) / ii32.max
+reference_signal = reference_signal.astype(np.float64) / ii32.max
+correction_signal = correction_signal.astype(np.float64) / ii32.max
+
+fig, axs = plt.subplots(3, 1)
+axs[0].plot(reference_signal)
+axs[0].set_title('Reference signal')
+axs[0].grid(True)
+axs[0].set_ylim(-1.0, 1.0)
+axs[1].plot(correction_signal)
+axs[1].set_title('Correction signal')
+axs[1].grid(True)
+axs[1].set_ylim(-1.0, 1.0)
+axs[2].plot(error_signal)
+axs[2].set_title('Error signal')
+axs[2].grid(True)
+axs[2].set_ylim(-1.0, 1.0)
+
+fig.tight_layout()
+plt.show()
