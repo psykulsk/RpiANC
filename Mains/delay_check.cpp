dd3d946ac75f929ce60d90b7b6b63806304d6fef
Piotr Sykulski <pitersk@gmail.com>
2019-03-09 12:17:52 +0100

    Parallel write and read in delay check

--- Mains/delay_check.cpp
+++ Mains/delay_check.cpp
@@ -8,6 +8,7 @@
 #include "../Headers/playback.h"
 #include "../Headers/processing.h"
 #include <algorithm>
+#include <omp.h>
 
 #define DEPLOYED_ON_RPI
 
@@ -17,7 +18,6 @@ long single_delay_check(long play_buffer_length, snd_pcm_uframes_t buffer_length
                         std::ifstream &noise_file) {
 
 
-    int sample = 0;
     std::chrono::high_resolution_clock::time_point start_time;
     std::chrono::high_resolution_clock::time_point end_time;
     bool start = true;
@@ -28,32 +28,53 @@ long single_delay_check(long play_buffer_length, snd_pcm_uframes_t buffer_length
 
     fixed_sample_type play_buffer[play_buffer_length];
     fixed_sample_type capture_buffer[buffer_length];
-    while (sample < 3000 && !peak_found) {
-        ++sample;
-        size_t size = play_buffer_length * sizeof(fixed_sample_type);
-        noise_file.read((char *) play_buffer, size);
-        if (start) {
-            start_time = std::chrono::high_resolution_clock::now();
-            start = false;
+    long delay_us;
+#pragma omp parallel sections
+    {
+#pragma omp section
+        {
+            printf ("id = %d, \n", omp_get_thread_num());
+            int sample = 0;
+            while (sample < 3000) {
+                ++sample;
+                size_t size = play_buffer_length * sizeof(fixed_sample_type);
+                noise_file.read((char *) play_buffer, size);
+                playback(play_handle, play_buffer, cap_period_size);
+            }
+
         }
-        playback(play_handle, play_buffer, cap_period_size);
-        capture(cap_handle, capture_buffer, cap_period_size);
-        for (unsigned long i = 0; i < buffer_length; ++i) {
-            if (i % 2)
-                if (std::abs(capture_buffer[i]) >
-                    std::numeric_limits<fixed_sample_type>::max() * 0.1) {
-                    end_time = std::chrono::high_resolution_clock::now();
-                    peak_found = true;
-                    std::cout << "Peak found" << std::endl;
-                    break;
+#pragma omp section
+        {
+            printf ("id = %d, \n", omp_get_thread_num());
+            int sample = 0;
+            while (sample < 3000 && !peak_found) {
+                ++sample;
+                if (start) {
+                    start_time = std::chrono::high_resolution_clock::now();
+                    start = false;
                 }
+                capture(cap_handle, capture_buffer, cap_period_size);
+                for (unsigned long i = 0; i < buffer_length; ++i) {
+                    if (i % 2)
+                        if (std::abs(capture_buffer[i]) >
+                            std::numeric_limits<fixed_sample_type>::max() * 0.08) {
+                            end_time = std::chrono::high_resolution_clock::now();
+                            peak_found = true;
+                            std::cout << "Peak found" << std::endl;
+                            break;
+                        }
+                }
+            }
+            if (peak_found) {
+                delay_us = std::chrono::duration_cast<std::chrono::microseconds>(
+                        end_time - start_time).count();
+                std::cout << "Delay: " << delay_us << " us" << std::endl;
+            }
         }
     }
 
+
     if (peak_found) {
-        long delay_us = std::chrono::duration_cast<std::chrono::microseconds>(
-                end_time - start_time).count();
-        std::cout << "Delay: " << delay_us << " us" << std::endl;
         return delay_us;
     } else {
         std::cout << "Peak not found" << std::endl;
@@ -62,17 +83,17 @@ long single_delay_check(long play_buffer_length, snd_pcm_uframes_t buffer_length
 
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
-    if(argc < 2){
+    if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <NUMBER_OF_TEST_REPETITIONS>" << std::endl;
         return 1;
     }
 
     unsigned long number_of_tests;
     try {
-       number_of_tests = std::stoul(argv[1], nullptr);
-    } catch(std::invalid_argument const &e) {
+        number_of_tests = std::stoul(argv[1], nullptr);
+    } catch (std::invalid_argument const &e) {
         std::cerr << "Usage: " << argv[0] << " <NUMBER_OF_TEST_REPETITIONS>" << std::endl;
         return 1;
     }
@@ -107,10 +128,9 @@ int main(int argc, char* argv[]) {
                   &play_device_buffer, number_of_channels, playback_device_name);
     snd_pcm_uframes_t play_buffer_length = play_period_size * number_of_channels;
 
-    fixed_sample_type play_buffer[play_buffer_length];
     fixed_sample_type capture_buffer[buffer_length];
     std::vector<long> delay_test_results;
-    std::ifstream noise_file("short_tone.raw", std::ios::binary);
+    std::ifstream noise_file("tone.wav", std::ios::binary | std::ios::in);
     assert(noise_file.is_open());
 
     for (unsigned long i = 0; i < number_of_tests; ++i) {
@@ -120,7 +140,7 @@ int main(int argc, char* argv[]) {
             delay_test_results.push_back(delay);
         }
         usleep(500000);
-        for(int j=0; j<3000; j++){
+        for (int j = 0; j < 3000; j++) {
             capture(cap_handle, capture_buffer, cap_period_size);
         }
     }
